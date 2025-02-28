dcf3588bbe97324360d7bd71e27b150e0986b63a
EZWP123 <somerajodeedwayne@gmail.com>
2025-02-28 09:00:35 +0800

    newupdate

--- src/admin/adminDashboard.java
+++ src/admin/adminDashboard.java
@@ -72,7 +72,7 @@ public class adminDashboard extends javax.swing.JFrame {
 
         jLabel1.setFont(new java.awt.Font("Arial", 0, 24)); // NOI18N
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel1.setText("Welcome to User Dashboard");
+        jLabel1.setText("Welcome to Admin Dashboard");
         Header.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 0, 840, 80));
 
         Mainpanel.add(Header, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1010, 80));
